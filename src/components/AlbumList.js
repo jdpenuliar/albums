79880f1095ffe0f07480f9a9e2de9ac90b6545a0
jdpenuliar <jaydz.penuliar@gmail.com>
2016-10-17 13:53:41 -0700

    add states

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,14 +1,81 @@
-import React from 'react';
+//import Component for extend without destructuring import
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-     return (
-          <View>
-               <Text>
-                    Album List!!!! haha
-               </Text>
-          </View>
-     );
-};
+//classes do not require semi colon
+
+
+class AlbumList extends Component {
+     // Class Component - used for dynamic sources of data
+     // Class Component - handles any data that might change (fetching data, user events, etc)
+     // Class Component - More code to write.
+     // Class Component - lifecycle method - functions that are
+     // placed in a class that automatically will be called at
+     // some point (trigger). Once called “component will mount
+
+     ///////////////////////////////////////////////
+     //          setting states from react        //
+     ///////////////////////////////////////////////
+
+     //class level property
+
+     // step 1 initial state or empty state
+     state = { albums: [] };
+
+     //lifecycle methods
+
+     componentWillMount() {
+          // can pause the run time. chrome will enter
+          // debugger mode.
+          // debugger;
+          console.log('haha header');
+
+          //to request from api npm install axios. Import it
+          // it has promise.
+          // api has to be https.
+
+          // React Native boots up
+          // RN decides to render ‘app’ to the screen
+          // App decides to render it self, Header and AlbumList
+          // Album list hits lifecycle componentwillmount.
+          // ajax call
+          // app, albumlist, header appear on screen but with no data
+          // time has passed but only then ajax response return json
+          //
+          // THIS WOULD BE SOLVE BY ‘component state’ by react
+
+
+          // axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+          // .then(response => console.log(response));
+
+          //step 2
+          // this. setState is a function from Component to update components
+          // for rerender
+          axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+          .then(response => this.setState({ albums: response.data }));
+
+          // Rules of State
+          // Definition of state: a plain javascript object used
+          //to record and respond to user-triggered events
+          // When we need to update what a component shows, call ‘this.setState’
+          // Only change state with ‘setState;, do not do ‘this.state’
+          //
+          // Prop - communication between parent to child communication
+          // State - component internal record keeping. Update
+          // data in a component. Only for class based components
+     }
+
+     render() {
+          console.log(this.state);
+          return (
+               <View>
+                    <Text>
+                         Album List!!!! haha
+                    </Text>
+               </View>
+          );
+     }
+}
 
 export default AlbumList;
